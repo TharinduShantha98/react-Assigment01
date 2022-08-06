c53c75bb25736bffc0968bd91e36e85c549c3927
TharinduShantha98 <tharindushantha4@gmail.com>
2022-08-06 12:15:36 +0530

    created user registraation page

--- src/pages/UserRegistration/UserRegistration.jsx
+++ src/pages/UserRegistration/UserRegistration.jsx
@@ -1,13 +1,39 @@
 import React, {Component} from "react";
 import {styleSheet} from "./index";
 import withStyles from "@material-ui/core/styles/withStyles";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import LockIcon from "@material-ui/icons/Lock";
+import Box from "@material-ui/core/Box";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import TableContainer from "@material-ui/core/TableContainer";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import Paper from "@material-ui/core/Paper";
 
 class UserRegistration extends Component{
 
     constructor(props) {
         super(props);
 
+
+
+        this.state= {
+
+
+            data: [],
+        }
+
     }
+
+
+
+
 
 
     render() {
@@ -16,16 +42,308 @@ class UserRegistration extends Component{
 
 
 
-        return(
-            <div className={classes.container}>
+
+
+            return(
+                <div className={classes.container}>
+                    <div className={classes.container_div}>
+
+                        <div className={classes.container_div_div1}>
+
+                            <div className={classes.container_div_div1_div1}>
+                                {/*<DriveEtaIcon color={"success"}  className={classes.container_div1_icon}/>*/}
+                                <Typography component="h1" variant="h5">
+                                   User Registration
+                                </Typography>
+                            </div>
+
+                            <div className={classes.container_div_div1_div2}>
+                                <div className={classes.container_div_div1_div2_div1}>
+
+
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="FirstName"
+                                        label="FirstName"
+                                        name="FirstName"
+                                        //  autoComplete="email"
+                                        variant="outlined"
+                                        size={'small'}
 
 
 
-            </div>
+                                    />
+
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                        variant="outlined"
+                                        autoFocus
+                                        size={"small"}
+
+
+                                    />
+
+
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        variant="outlined"
+                                        autoComplete="current-password"
+                                        // InputProps={{
+                                        //     startAdornment: (
+                                        //         <InputAdornment position="start">
+                                        //             <LockIcon />
+                                        //         </InputAdornment>
+                                        //     ),
+                                        // }}
+
+                                        size={"small"}
 
 
 
-        )
+                                    />
+
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="street"
+                                        label="street"
+                                        name="street"
+                                        //  autoComplete="email"
+                                        variant="outlined"
+                                        size={'small'}
+
+
+                                    />
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="ZipCode"
+                                        label="Zip code"
+                                        name="ZipCode"
+                                        variant="outlined"
+                                        size={'small'}
+
+
+                                    />
+
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="longValue"
+                                        label="longValue"
+                                        name="longValue"
+                                        variant="outlined"
+                                        size={'small'}
+
+
+                                    />
+
+                                </div>
+                                <div  className={classes.container_div_div1_div2_div2}>
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="lastName"
+                                        label="lastName"
+                                        name="lastName"
+                                        //  autoComplete="email"
+                                        variant="outlined"
+                                        size={'small'}
+
+                                    />
+
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="userName"
+                                        label="userName"
+                                        name="userName"
+                                        //  autoComplete="email"
+                                        variant="outlined"
+                                        size={'small'}
+
+                                    />
+
+
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="city"
+                                        label="city"
+                                        name="city"
+                                        // autoComplete="carType"
+                                        variant="outlined"
+                                        size={'small'}
+
+
+
+
+                                    />
+
+
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="streetNo"
+                                        label="streetNo"
+                                        name="streetNo"
+                                        //  autoComplete="email"
+                                        variant="outlined"
+                                        size={'small'}
+
+                                    />
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="lastValue"
+                                        label="lastValue"
+                                        name="lastValue"
+                                        //  autoComplete="email"
+                                        variant="outlined"
+
+                                        size={'small'}
+
+
+                                    />
+
+
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="mobile No"
+                                        label="mobile No"
+                                        name="mobile No"
+                                        //  autoComplete="email"
+                                        variant="outlined"
+
+                                        size={'small'}
+
+
+                                    />
+
+
+                                </div>
+
+
+                            </div>
+
+                            <div className={classes.container_div_div1_div3}>
+                                <div className={classes.container_div_div1_div4}>
+                                    <Button
+                                        variant="outlined"
+                                        color="primary"
+                                        style={{marginLeft:"5px",
+                                        width: "20%",
+                                        }}
+                                    >
+                                        Clear
+                                    </Button>
+                                    <Button
+                                        variant="outlined"
+                                        color="primary"
+                                        style={{marginLeft:"5px",
+                                            width: "20%",
+                                        }}
+                                    >
+                                        Save
+                                    </Button>
+                                </div>
+
+
+
+
+
+
+                            </div>
+
+
+                        </div>
+
+
+
+                    </div>
+
+
+
+                    <div className={classes.container_div2}>
+
+                        <TableContainer component={Paper}>
+                            <Table className={classes.table} aria-label="simple table">
+                                <TableHead style={{backgroundColor:"#e26868",
+
+                                }}>
+                                    <TableRow >
+                                        <TableCell style={{fontWeight:"bold",
+                                                        width:"50% !important",
+                                           // backgroundColor:"#f3e8e8"
+
+                                        }}
+                                                   align="right"
+
+                                        >FirstName</TableCell>
+                                        <TableCell style={{fontWeight:"bold"}} align="right">LastName</TableCell>
+                                        <TableCell style={{fontWeight:"bold"}} align="right">Email address</TableCell>
+                                        <TableCell style={{fontWeight:"bold"}} align="right">userName</TableCell>
+                                        <TableCell style={{fontWeight:"bold"}} align="right">password</TableCell>
+                                        <TableCell style={{fontWeight:"bold"}} align="right">city</TableCell>
+                                        <TableCell style={{fontWeight:"bold"}} align="right">street</TableCell>
+                                        <TableCell style={{fontWeight:"bold"}} align="right">street No</TableCell>
+                                        <TableCell style={{fontWeight:"bold"}} align="right">ZipCode</TableCell>
+                                        <TableCell style={{fontWeight:"bold"}} align="right">lastValue</TableCell>
+                                        <TableCell style={{fontWeight:"bold"}} align="right">LongValue</TableCell>
+                                        <TableCell style={{fontWeight:"bold"}} align="right">password</TableCell>
+                                        <TableCell style={{fontWeight:"bold"}} align="right">Update</TableCell>
+                                        <TableCell style={{fontWeight:"bold"}} align="right">Delete</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {this.state.data.map((row) => (
+                                        <TableRow key={row.name}>
+                                            <TableCell component="th" scope="row">
+                                                {row.name}
+                                            </TableCell>
+                                            {/*<TableCell align="right">{row.calories}</TableCell>*/}
+                                            {/*<TableCell align="right">{row.fat}</TableCell>*/}
+                                            {/*<TableCell align="right">{row.carbs}</TableCell>*/}
+                                            {/*<TableCell align="right">{row.protein}</TableCell>*/}
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+
+
+                    </div>
+
+
+                </div>
+
+
+
+            )
     }
 
 
--- src/pages/UserRegistration/index.js
+++ src/pages/UserRegistration/index.js
@@ -1,13 +1,102 @@
 export const styleSheet={
     container:{
-        width:"100vw",
-        height:'100vh',
-        backgroundColor:"#faf8f8",
+     //   width:"100vw",
+        height:'200vh',
+  //      backgroundColor:"#ec9f9f",
         display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+        flexDirection:'column',
+        // alignItems: "center",
+        // justifyContent: "center",
+
+    },
+    container_div:{
+        width: '100%',
+        height: '110vh',
+        //    backgroundColor:'#ef6e6e',
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    container_div_div1:{
+        width: '70%',
+        height: '100vh',
+        //  backgroundColor:'#cd2d2d',
+        display:'flex',
+        //alignItems:'center',
+        //justifyContent:'center',
+        flexDirection:'column'
 
 
+    },
+    container_div_div1_div1:{
+        width: '100%',
+        height: '10vh',
+        //     backgroundColor:'#2dcd50',
+        display:'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
 
+
+    },
+    container_div1_icon:{
+        fontSize:'50px !important',
+
+    },
+    container_div_div1_div2:{
+        width: '100%',
+        height: '80%',
+  //         backgroundColor:'#2d7acd',
+        display:'flex',
+        flexDirection: 'row',
+
+    },
+    container_div_div1_div2_div1:{
+        width:'50%',
+        height:'100%',
+        //      backgroundColor:'#8acd2d',
+        display:'flex',
+        flexDirection:'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+
+    container_div_div1_div2_div2:{
+        width:'50%',
+        height:'100%',
+        //     backgroundColor:'#2d88cd',
+        display:'flex',
+        flexDirection:'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container_div_div1_div3:{
+        width: '100%',
+        height: '20%',
+ //       backgroundColor:'#eee3eb',
+        display:'flex',
+        flexDirection:'row',
+
+    },
+    container_div_div1_div4:{
+        width: '100%',
+        height: '10vh',
+  //      backgroundColor:'#2dcd50',
+        display:'flex',
+        alignItems: 'center',
+        justifyContent: 'right',
+        flexDirection:'row',
+
+
+    },
+
+    container_div2:{
+
+        width:"100%",
+        height:'100vh',
+ //       backgroundColor:"#e23e3e"
     }
+
+
+
 }
